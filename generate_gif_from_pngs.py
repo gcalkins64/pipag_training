eb0cfccde9cf2fd0cb2ebc94c805c655f7a2a572
Grace Calkins <grca5156@colorado.edu>
2025-03-20 21:57:13 -0600

    Added LD 6

--- generate_gif_from_pngs.py
+++ generate_gif_from_pngs.py
@@ -8,9 +8,30 @@ import imageio
 # LD 4 NC 3
 # folder_path = '/Users/gracecalkins/Local_Documents/local_code/pipag_training/data/gmvae_em_aerocapture_energy_20250320_192010'
 # LD 4 NC 4
-folder_path = '/Users/gracecalkins/Local_Documents/local_code/pipag_training/data/gmvae_em_aerocapture_energy_20250320_204111'
+# folder_path = '/Users/gracecalkins/Local_Documents/local_code/pipag_training/data/gmvae_em_aerocapture_energy_20250320_204111'
 # LD 4 NC 5
-folder_path = '/Users/gracecalkins/Local_Documents/local_code/pipag_training/data/gmvae_em_aerocapture_energy_20250320_223846'
+# folder_path = '/Users/gracecalkins/Local_Documents/local_code/pipag_training/data/gmvae_em_aerocapture_energy_20250320_223846'
+# LD 4 NC 6
+# folder_path = '/Users/gracecalkins/Local_Documents/local_code/pipag_training/data/gmvae_em_aerocapture_energy_20250320_235238'
+# LD 5 NC 2
+folder_path = '/Users/gracecalkins/Local_Documents/local_code/pipag_training/data/gmvae_em_aerocapture_energy_20250321_011131'
+# LD 5 NC 3
+# folder_path = '/Users/gracecalkins/Local_Documents/local_code/pipag_training/data/gmvae_em_aerocapture_energy_20250321_011120'
+# LD 5 NC 4
+
+# LD 5 NC 5
+
+# LD 5 NC 6
+
+# LD 6 NC 2
+
+# LD 6 NC 3
+
+# LD 6 NC 4
+
+# LD 6 NC 5
+
+# LD 6 NC 6
 
 # Regular expression to extract epoch number XXXXX from filename
 pattern = re.compile(r"latent_samples_epoch_(\d+)_.*")
