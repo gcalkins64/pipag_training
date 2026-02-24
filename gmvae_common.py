645ef4eb1ffbbeb12d9dbb1a1725431168fda121
Grace Calkins <grca5156@colorado.edu>
2026-02-24 14:13:28 -0700

    removed latent plotting printing

--- gmvae_common.py
+++ gmvae_common.py
@@ -45,7 +45,7 @@ def plot_latent_space_with_clusters(samples, labels, num_clusters, cluster_means
     unique_labels = torch.unique(labels)
     print(len(unique_labels))
     for i in range(len(unique_labels)):
-        print(i)
+        # print(i)
         this_label = unique_labels[i]
         samples_i = samples_[labels == this_label]
         if samples_i.shape[0] > 0:
